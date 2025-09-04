6f31d50f83421037afa0c855f5674dcf27bc771a
Zero <zerodev.exe@proton.me>
2025-09-04 14:06:32 -0400

    Add StopLossRequest import and refactor order functions in make_orders.py

--- make_orders.py
+++ make_orders.py
@@ -1,5 +1,5 @@
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import MarketOrderRequest, LimitOrderRequest
+from alpaca.trading.requests import MarketOrderRequest, LimitOrderRequest, StopLossRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 from dotenv import load_dotenv
 import os
@@ -12,8 +12,6 @@ SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
 trading_clinet = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
-print(trading_clinet.get_account().buying_power)
-
 def get_account():
     return trading_clinet.get_account().buying_power
 
@@ -21,24 +19,21 @@ def get_orders():
     return trading_clinet.get_orders()
 
 def get_positions():
-    return trading_clinet.get_positions()
-
-def get_open_positions():
-    return trading_clinet.get_open_positions()
-
-def get_closed_positions():
-    return trading_clinet.get_closed_positions()
+    return trading_clinet.get_all_positions()
 
 def make_market_order(symbol, qty, side):
-    order_data = MarketOrderRequest(
-        symbol=symbol,
-        qty=qty,
-        side=side,
-        time_in_force=TimeInForce.DAY,
-    )
-    market_order = trading_clinet.submit_order(order_data)
-    print(market_order)
-    return trading_clinet.submit_order(order_data)
+    try:
+        order_data = MarketOrderRequest(
+            symbol=symbol,
+            qty=qty,
+            side=side,
+            time_in_force=TimeInForce.DAY,
+        )
+        market_order = trading_clinet.submit_order(order_data)
+        print(market_order)
+        return trading_clinet.submit_order(order_data)
+    except Exception as e:
+        print(f"Error processing trade: {str(e)}")
 
 def make_limit_order(symbol, qty, side, limit_price):
     order_data = LimitOrderRequest(
@@ -53,4 +48,6 @@ def make_limit_order(symbol, qty, side, limit_price):
     print(limit_order)
 
 if __name__ == "__main__":
-    get_account()
+    print(get_account())
+    print(get_orders())
+    print(get_positions())
