49ba648773d3d628cf76eb61a80e1ec4e6fc42b0
Zero <zerodev.exe@proton.me>
2025-09-11 11:41:57 -0400

    Refactor order functions: remove stop and take order implementations, enhance market order with take profit and stop loss parameters

--- make_orders.py
+++ make_orders.py
@@ -1,14 +1,8 @@
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest, LimitOrderRequest, StopLossRequest, TakeProfitRequest
-from alpaca.trading.enums import OrderSide, TimeInForce
-from dotenv import load_dotenv
-import os
+from alpaca.trading.enums import OrderSide, TimeInForce, OrderClass
+from parameters import API_KEY, SECRET_KEY
 
-load_dotenv()
-
-# Get API credentials from environment variables
-API_KEY = os.getenv("ALPACA_API_KEY")
-SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
 trading_clinet = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
@@ -24,47 +18,21 @@ def get_open_positions(symbol: str):
 def get_positions():
     return trading_clinet.get_all_positions()
 
-def make_market_order(symbol, qty, side):
+def make_market_order(symbol, qty, side, take_profit=None, stop_loss=None):
     try:
         order_data = MarketOrderRequest(
             symbol=symbol,
             qty=qty,
             side=side,
             time_in_force=TimeInForce.DAY,
+            order_class=OrderClass.BRACKET,
+            take_profit=TakeProfitRequest(take_profit),
+            stop_loss=StopLossRequest(stop_loss)
         )
         market_order = trading_clinet.submit_order(order_data)
-        print(market_order)
-        return trading_clinet.submit_order(order_data)
+        return market_order
     except Exception as e:
         print(f"Error processing trade: {str(e)}")
-
-def make_stop_order(symbol, qty, side, stop_price):
-    try:
-        order_data = StopLossRequest(
-            symbol=symbol,
-            qty=qty,
-            side=side,
-            time_in_force=TimeInForce.DAY,
-            stop_price=stop_price,
-        )
-        stop_order = trading_clinet.submit_order(order_data)
-    except Exception as e:
-        print(f"Error processing trade: {str(e)}")
-        print(stop_order)
-
-def make_take_order(symbol, qty, side, take_price):
-    try:
-        order_data = TakeProfitRequest(
-            symbol=symbol,
-            qty=qty,
-            side=side,
-            time_in_force=TimeInForce.DAY,
-            take_price=take_price,
-        )
-        take_order = trading_clinet.submit_order(order_data)
-    except Exception as e:
-        print(f"Error processing trade: {str(e)}")
-        print(take_order)
 
 if __name__ == "__main__":
     print(get_account())
