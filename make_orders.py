7f375d739b5ad49642db2c04d46c05a34dc5c19f
Zero <zerodev.exe@proton.me>
2025-09-11 12:03:00 -0400

    Refactor make_market_order function: improve type annotations and enhance order data structure

--- make_orders.py
+++ make_orders.py
@@ -18,18 +18,19 @@ def get_open_positions(symbol: str):
 def get_positions():
     return trading_clinet.get_all_positions()
 
-def make_market_order(symbol, qty, side, take_profit=None, stop_loss=None):
+def make_market_order(symbol: str, qty: int, side, take_profit: float, stop_loss: float):
     try:
-        order_data = MarketOrderRequest(
-            symbol=symbol,
-            qty=qty,
-            side=side,
-            time_in_force=TimeInForce.DAY,
-            order_class=OrderClass.BRACKET,
-            take_profit=TakeProfitRequest(take_profit),
-            stop_loss=StopLossRequest(stop_loss)
-        )
-        market_order = trading_clinet.submit_order(order_data)
+        bracket__order_data = MarketOrderRequest(
+                    symbol=symbol,
+                    qty=qty,
+                    side=side,
+                    time_in_force=TimeInForce.DAY,
+                    order_class=OrderClass.BRACKET,
+                    take_profit=TakeProfitRequest(limit_price=take_profit),
+                    stop_loss=StopLossRequest(stop_price=stop_loss)
+                    )
+
+        market_order = trading_clinet.submit_order(bracket__order_data)
         return market_order
     except Exception as e:
         print(f"Error processing trade: {str(e)}")
